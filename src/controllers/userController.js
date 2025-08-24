f295a6c4c9406e0ac253f0c40d39876f96d2fc6c
jaypanchal <jay@bugsfreeinnovation.com>
2025-08-25 00:45:51 +0530

    hoho

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -7,7 +7,7 @@ export const getAllUsers = async (req, res) => {
             ? { $or: [{ username: { $regex: q, $options: 'i' } }, { email: { $regex: q, $options: 'i' } }] }
             : {};
         const users = await User.find(filter).select('-password');
-        res.json(users);
+        res.json({data : users});
     } catch (err) {
         res.status(500).json({ message: err.message });
     }
